8c6997e606f9945c8bb9845662796aaa8c45be9f
saulusribeiro <saulusribeiro@gmail.com>
2023-04-25 19:33:27 -0300

    Primeiro Commit Projeto Backend Controle de Pedidos

---
+++ ControlePedidosBackend/src/main/java/com/saulusribeiro/ControlePedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saulusribeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePedidosBackendApplication.class, args);
+	}
+
+}
