d4fdbaba0e45f84e1d34c2931dab8ecc7b7dedd2
saulusribeiro <saulusribeiro@gmail.com>
2023-04-28 10:56:44 -0300

    Criando operação de Instaciação

--- ControlePedidosBackend/src/main/java/com/saulusribeiro/controlepedidos/ControlePedidosBackendApplication.java
+++ ControlePedidosBackend/src/main/java/com/saulusribeiro/controlepedidos/ControlePedidosBackendApplication.java
@@ -1,13 +1,36 @@
 package com.saulusribeiro.controlepedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ControlePedidosBackendApplication {
+import com.saulusribeiro.controlepedidos.domain.Categoria;
+import com.saulusribeiro.controlepedidos.respositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ControlePedidosBackendApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(ControlePedidosBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+
+		
 	}
 
 }
--- ControlePedidosBackend/src/main/java/com/saulusribeiro/controlepedidos/respositories/CategoriaRepository.java
+++ ControlePedidosBackend/src/main/java/com/saulusribeiro/controlepedidos/respositories/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package com.saulusribeiro.controlepedidos.respositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.saulusribeiro.controlepedidos.domain.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
 
 }
